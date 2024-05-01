14c8f5090961ca1b64067d048c203973d360e7e0
liatfa <faizliat53@gmail.com>
2024-05-01 19:17:58 +0300

    leptop push

--- src/App.js
+++ src/App.js
@@ -5,20 +5,28 @@ import Intro from './components/Intro';
 import Services from './components/Services';
 import Title from './components/Title';
 import BackgroundSection from './components/BackgroundSection';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
 
+<Router>
+<Routes>
 
-       <BackgroundSection>
-       <Dashboard/>
-       <Title/>
-      </BackgroundSection>
+<Route path="/" element={<BackgroundSection />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/title" element={<Title />} />
+          </Route>
 
-   <Intro/>
-   <Services/>
-   <Contact/>
+  <Route path='/intro' element={<Intro/>}/>
+  <Route path='/services' element={<Services/>}/>
+  <Route path='/contact' element={<Contact/>}/>
+</Routes>
+</Router>
+
+  
+
     </div>
   );
 }
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
 
@@ -24,9 +25,14 @@ const Dashboard = () => {
 
   const dashboardStyle = {
     backgroundColor: isScrolled ? 'white' : 'transparent',
-    position: 'sticky',
-    top: 0,
+    transition: 'background-color 0.5s ease, height 0.5s ease',
     zIndex: 999,
+    position: isScrolled ? 'fixed' : 'relative', // Change position to fixed when scrolled
+    top: isScrolled ? 0 : 'auto',
+    left: 0,
+    right: 0,
+    height: isScrolled ? '80px' : '150px',
+   
   };
 
   const dashboardBtnStyle = {
@@ -44,14 +50,14 @@ const Dashboard = () => {
   return (
     <div className='dashboard' style={dashboardStyle}> 
         <div className='btns'>
-        <button id='btn'  style={dashboardBtnStyle}>בית</button>
-        <button id='btn'  style={dashboardBtnStyle}>אודות</button>
-        <button id='btn'  style={dashboardBtnStyle}>שירותים</button>
-        <button id='btn'  style={dashboardBtnStyle}>צור קשר</button>
+       <Link to='/title'> <button id='btn'  style={dashboardBtnStyle}>בית</button> </Link>
+       <Link to='/intro'> <button id='btn'  style={dashboardBtnStyle}>אודות</button> </Link> 
+       <Link to='/services'> <button id='btn'  style={dashboardBtnStyle}>שירותים</button> </Link> 
+       <Link to='/contact'> <button id='btn'  style={dashboardBtnStyle}>צור קשר</button>  </Link> 
     </div>
 
     <div id='miniTitle' style={dashboardTitleStyle}>
-        <p>Ozar-Psychotherapy</p>
+        Ozar-Psychotherapy
         </div>
     </div>
     
